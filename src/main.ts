3e07113d444e17f560710b24f7cf409218c1aedb
erkelost <1256029807@qq.com>
2022-03-29 13:47:08 +0800

    chore(build): 打包合并✨✨

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+// page 文件路由系统
+// import { createRouter, createWebHistory } from 'vue-router'
+// import routes from 'virtual:generated-pages'
 import App from './App.vue'
+import router from '~/router/index'
+import { setupStore } from '~/store'
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import './styles/nprogress.scss'
+import 'uno.css'
+console.log(router)
 
-createApp(App).mount('#app')
+const app = createApp(App)
+// page 文件路由系统
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes
+// })
+app.use(router)
+setupStore(app)
+app.mount('#app')
